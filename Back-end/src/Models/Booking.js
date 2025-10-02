d533adfb5a1bfedc4e7a34ca532237b369aeb3a7
s1ad <iflisaad17@gmaiL.com>
2025-10-02 23:20:46 +0100

    Booking schema

---
+++ Back-end/src/Models/Booking.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bookingSchema = new Schema({
+  userID: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  tripID: {
+    type: Schema.Types.ObjectId,
+    ref: 'TripPack',
+    required: true
+  },
+  confirmation_number: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  isCancelled: {
+    type: Boolean,
+    default: false
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
