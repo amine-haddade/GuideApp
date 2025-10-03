4f5fb62f061d5525420d35c0c2afaf989a26c0a8
amine haddade <aminehddade479@gmail.com>
2025-10-03 10:55:11 +0100

    add config witch database in mongo atlas and add colab teams

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -1,12 +1,12 @@
 // src/server.js
 import express from "express";
 import dotenv from "dotenv";
-
+import connectDB from "./Config/db.js";
 // Charger les variables d'environnement depuis .env
 dotenv.config();
 
 const app = express();
-
+connectDB()
 // Récupérer le port depuis le fichier .env, sinon utiliser 3000 par défaut
 const port = process.env.PORT || 3000;
 
