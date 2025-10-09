552ac9b1ed709c6d43ae2d5a1b89d06eec3c6dd2
amine haddade <aminehddade479@gmail.com>
2025-10-09 14:47:17 +0100

    setupt Redux React-router and layouts

--- Back-end/src/Routes/authRoutes.js
+++ Back-end/src/Routes/authRoutes.js
@@ -4,14 +4,18 @@ import {
   login,
   logout,
   refreshAccessToken,
+  updateProfile,
 } from "../Controllers/authController.js";
 import { createUserValidation } from "../Validations/userValidator.js";
 import { validate } from "../Middlewares/userValidate.js";
 import { verifyToken } from "../Middlewares/verifyJwtToken.js";
+import { updateProfileValidation } from "../Validations/authValidator.js";
+import { authorizeRoles } from "../Middlewares/authorizeRole.js";
 
 const router = express.Router();    
 
 router.post("/signup", createUserValidation, validate, signUp);
+router.put('/profile', verifyToken, authorizeRoles(["client", "guide"]), updateProfileValidation, validate, updateProfile)
 router.post("/login", login);
 router.post("/refresh", refreshAccessToken);
 router.post("/logout", verifyToken, logout);  
